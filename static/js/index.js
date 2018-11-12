9c0386f53885f2edbd8a81d7a4a3d9c3a6120f24
Austin Kellar <akellar@ucsc.edu>
2018-11-12 14:38:11 -0800

    deleted comments

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,5 @@
-// here, we declare a global variable
-// in web2py will translate undefined (JS) to None, so we define this variable for convenience
-// it is important to note that null (JS) gets translated to "" in python, NOT None.
-// so we have to use undefined here
 const None = undefined;
 
-// Enumerates an array.
-// this will give an _idx attribute to each movie
-// the _idx will be assigned 0, 1, 2, 3 ...
 var enumerate = function(arr) { 
     var k=0; return arr.map(function(e) {
         e._idx = k++;
@@ -34,11 +27,9 @@ var insertMovie = function() {
         rating: app.newMovieRating
     };
     $.post(insertMoviesUrl, newMovie, function(response) { 
-        // the server responded with the id number of the new movie in the database. make sure to add this to the
-        // new movie object before we add it to the view
         newMovie['id'] = response.new_movie_id;
         app.movies.push(newMovie);
-        processMovies(); // ned to re-index the movies now that a new one has been added to thea array
+        processMovies(); 
     });
 };
 
@@ -51,8 +42,6 @@ var saveMovie = function(idx) {
     // make request to server here to save changes in database
 };
 
-// here, we define the Vue variable. Remember, only the fields defined here (in data and methods) are 
-// available inside the html
 var app = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
